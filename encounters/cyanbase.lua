9930bdbc33747544e84eff141aff46c8f79b98c5
CyanmanEXE <124416491+CyanmanEXE@users.noreply.github.com>
2023-09-12 01:35:39 -0400

    1.5(bear with me)
    testing Cirkill

--- encounters/cyanbase.lua
+++ encounters/cyanbase.lua
@@ -22,14 +22,28 @@ local encounter1 = {
     
 }
 
-
+local encounter2 = {
+    path="/server/assets/ezlibs-assets/ezencounters/ezencounters.zip",
+    weight=90,
+    enemies = {
+        {name="CirKill",rank=1},
+       
+    },
+    positions = {
+        
+        {0,0,0,0,0,0},
+        {0,0,0,0,1,0},
+        {0,0,0,0,0,0}
+    },
+    
+}
 
 
 
 return {
     minimum_steps_before_encounter=90,
     encounter_chance_per_step=0.9,
-    encounters={encounter1}
+    encounters={encounter1, encounter2}
 }
 
 
