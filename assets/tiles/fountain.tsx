8cade31a57fe68b3d6096ef4b1334b3b85445081
CyanmanEXE <124416491+CyanmanEXE@users.noreply.github.com>
2023-09-25 21:22:56 -0400

    1.5.15
    Scenery improvment and added fixes somwhat

---
+++ assets/tiles/fountain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="fountain" tilewidth="64" tileheight="32" tilecount="42" columns="6">
+ <image source="fountain.png" width="408" height="232"/>
+</tileset>
---
+++ assets/tiles/kabob.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="Stalls - Kabob" tilewidth="64" tileheight="52" tilecount="1" columns="1" objectalignment="bottom">
+ <grid orientation="isometric" width="64" height="32"/>
+ <image source="kabob.png" width="64" height="52"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
