416f9633154c72dcfb530469ed7f485bbddfbd39
CyanmanEXE <124416491+CyanmanEXE@users.noreply.github.com>
2023-09-28 09:55:21 -0400

    1.5.18
    Huge changes from new keys, new coding to shrink player, gates and warps!

--- scripts/ezlibs-custom/custom.lua
+++ scripts/ezlibs-custom/custom.lua
@@ -2,5 +2,6 @@ local CustPlugin = {}
 
 local helpers = require('scripts/ezlibs-scripts/helpers')
 
+local compression = helpers.safe_require('scripts/ezlibs-custom/compression')
 
 return CustPlugin
