bc3bb195ea87916d3ba30f796813ecc15a142e3a
CyanmanEXE <124416491+CyanmanEXE@users.noreply.github.com>
2023-10-15 18:01:02 -0400

    1.6.4
    Heavy changes made for libs and what not

--- scripts/ezlibs-custom/custom.lua
+++ scripts/ezlibs-custom/custom.lua
@@ -4,9 +4,7 @@ local CustPlugin = {}
 
 
 local helpers = require('scripts/ezlibs-scripts/helpers')
-
-local nebulibs = helpers.safe_require('scripts/ezlibs-custom/compression')
-
+local nebulibs = require('scripts/ezlibs-custom/nebulous-liberations/main')
 local compression = helpers.safe_require('scripts/ezlibs-custom/compression')
 
 return CustPlugin
