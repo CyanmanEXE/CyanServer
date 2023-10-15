db1b04def9dddd383a3b6aacc6adb3ada0f379a8
CyanmanEXE <124416491+CyanmanEXE@users.noreply.github.com>
2023-10-15 18:37:30 -0400

    1.6.5
    fixes i hope

--- assets/tiles/under_pine.tsx
+++ assets/tiles/under_pine.tsx
@@ -1,14 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="Nature - Undernet Pine" tilewidth="64" tileheight="67" tilecount="4" columns="4" objectalignment="bottom">
- <tileoffset x="0" y="16"/>
- <grid orientation="isometric" width="64" height="32"/>
+<tileset version="1.4" tiledversion="1.4.1" name="tree" tilewidth="44" tileheight="61" tilecount="4" columns="4" objectalignment="bottom">
+ <tileoffset x="0" y="12"/>
  <properties>
   <property name="Solid" type="bool" value="true"/>
  </properties>
- <image source="under_pine.png" width="256" height="67"/>
+ <image source="under_pine.png" width="176" height="61"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="-1" y="-1" width="34" height="34">
+   <object id="1" x="0.36607" y="41" width="41" height="19.1452">
     <ellipse/>
    </object>
   </objectgroup>
